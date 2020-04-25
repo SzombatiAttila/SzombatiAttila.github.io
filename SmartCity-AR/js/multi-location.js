46c56ae6fafa7fd30c08e8d8cc57a82e664222e9
Szombati András <bandi@Szombati-MacBook-Pro.local>
2020-04-25 17:30:17 +0200

    1st task

--- SmartCity-AR/js/multi-location.js
+++ SmartCity-AR/js/multi-location.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 47.474828,
+                lng: 19.039706,
             },
         },
     ];
